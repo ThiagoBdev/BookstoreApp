fd5d7bb6a8dad346dac9824934a15f08184256b4
thiago batista <thiagobatista15390@gmail.com>
2025-08-31 13:30:07 -0300

    ajustando mais

---
+++ bookstore/views.py
@@ -0,0 +1,22 @@
+from django.http import HttpResponse
+from django.template import loader
+from django.views.decorators.csrf import csrf_exempt
+
+import git
+
+
+@csrf_exempt
+def update(request):
+    import git
+    from django.http import JsonResponse
+
+    if request.method == "POST":
+        repo = git.Repo('/home/ThiagoBdev/BookstoreApp')  
+        origin = repo.remotes.origin
+        try:
+            origin.fetch()
+            repo.git.reset('--hard', 'origin/main')  
+            return JsonResponse({"msg": "Updated code on PythonAnywhere"})
+        except Exception as e:
+            return JsonResponse({"error": str(e)}, status=500)
+    return JsonResponse({"error": "Only POST allowed"}, status=405)
