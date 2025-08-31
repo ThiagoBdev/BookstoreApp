bb60e48b3e3876734d22362bbeb8e544ac169aa8
thiago batista <thiagobatista15390@gmail.com>
2025-08-31 14:11:00 -0300

    debugando

--- bookstore/views.py
+++ bookstore/views.py
@@ -4,22 +4,13 @@ from django.views.decorators.csrf import csrf_exempt
 
 import git
 
-
 @csrf_exempt
 def update(request):
-    import git
-    from django.http import JsonResponse
-
     if request.method == "POST":
-        repo = git.Repo('/home/ThiagoBdev/BookstoreApp')  
-        origin = repo.remotes.origin
-        try:
-            origin.fetch()
-            repo.git.reset('--hard', 'origin/main')  
-            return JsonResponse({"msg": "Updated code on PythonAnywhere"})
-        except Exception as e:
-            return JsonResponse({"error": str(e)}, status=500)
-    return JsonResponse({"error": "Only POST allowed"}, status=405)
+        print("Webhook received!")
+        print(request.body.decode())  # Para ver o JSON cru
+        return HttpResponse("Webhook received!")
+    return HttpResponse("Only POST allowed", status=405)
 
 
 def hello_world(request):
